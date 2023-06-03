af0003a2a735b24f594615efe2dd4af5642bdac4
KokondaNikhil <nikhilkumar20.1995@gmail.com>
2023-06-03 11:51:47 +0530

    this is my first commit

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,26 @@
+package api.endpoints;
+
+/*
+ *Swagger URI--> https://petstore.swagger.io
+ * 
+Create User	(Post):	https://petstore.swagger.io/v2/user
+Get User	(Get):	https://petstore.swagger.io/v2/user/{username}
+Update User	(Put):	https://petstore.swagger.io/v2/user/{username}
+Delete User (Delete):	Delete	https://petstore.swagger.io/v2/user/{username}
+ * 
+ */
+
+//Routes contains only url's
+public class Routes {
+
+	
+	public static String base_url="https://petstore.swagger.io/v2";
+
+	
+	//User module
+	public static String post_url=base_url+"/user";
+	public static String get_url=base_url+"/user/{username}";
+	public static String update_url=base_url+"/user/{username}";
+	public static String delete_url=base_url+"/user/{username}";
+
+}
